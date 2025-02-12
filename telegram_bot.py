0704568c807ed1da577feb7ccab0062dabc78b59
Andrea Pruccoli <andrea.pruccoli@maggioli.it>
2025-02-12 17:22:43 +0100

    chore: add todo

--- telegram_bot.py
+++ telegram_bot.py
@@ -15,7 +15,7 @@ baseUrl = "https://www.vinted.it/catalog?catalog[]=4&order=newest_first&brand_id
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-
+# TODO add commands for each brand?
 # add a keyword to the db
 async def add_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # query = context.args
