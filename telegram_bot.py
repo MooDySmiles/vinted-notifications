9d29c1e3218e163d1e2ead544ef570e8801761b7
Andrea Pruccoli <andrea.pruccoli@maggioli.it>
2025-02-12 17:22:14 +0100

    refact: remove unused functions/commands; sets default url for queries

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,14 @@
 from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import db, os, configuration_values, requests
-from pyVinted import Vinted, requester
+from pyVinted import Vinted
 from traceback import print_exc
 from asyncio import queues
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
 VER = "0.5.2.3"
+
+baseUrl = "https://www.vinted.it/catalog?catalog[]=4&order=newest_first&brand_ids[]="
 
 
 # verify if bot still running
@@ -16,11 +18,14 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 # add a keyword to the db
 async def add_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = context.args
-    if not query:
-        await update.message.reply_text('No query provided.')
-        return
-    query = query[0]
+    # query = context.args
+    # if not query:
+    #     await update.message.reply_text('No query provided.')
+    #     return
+    # query = query[0]
+    query = f"{baseUrl}1043"
+
+    print(f"Setting query for {query}")
     # Parse the URL and extract the query parameters
     parsed_url = urlparse(query)
     query_params = parse_qs(parsed_url.query)
@@ -98,80 +103,11 @@ def format_queries():
     return query_list
 
 
-async def create_allowlist(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    db.create_allowlist()
-    await update.message.reply_text(f'Allowlist created. Add countries by using /add_country "FR,BE,ES,IT,LU,DE etc."')
-
-
-async def delete_allowlist(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    db.delete_allowlist()
-    await update.message.reply_text(f'Allowlist deleted. All countries are allowed.')
-
-
-async def add_country(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    country = context.args
-    if not country:
-        await update.message.reply_text('No country provided')
-        return
-    # remove spaces
-    country = ' '.join(country).replace(" ", "")
-    if len(country) != 2:
-        await update.message.reply_text('Invalid country code')
-        return
-    # if already in allowlist
-    if country.upper() in (country_list := db.get_allowlist()):
-        await update.message.reply_text(f'Country "{country.upper()}" already in allowlist. Current allowlist: {country_list}')
-    else:
-        db.add_to_allowlist(country.upper())
-        await update.message.reply_text(f'Done. Current allowlist: {db.get_allowlist()}')
-
-
-async def remove_country(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    country = context.args
-    if not country:
-        await update.message.reply_text('No country provided')
-        return
-    # remove spaces
-    country = ' '.join(country).replace(" ", "")
-    if len(country) != 2:
-        await update.message.reply_text('Invalid country code')
-        return
-    db.remove_from_allowlist(country.upper())
-    await update.message.reply_text(f'Done. Current allowlist: {db.get_allowlist()}')
-
-
-async def allowlist(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if db.get_allowlist() == 0:
-        await update.message.reply_text(f'No allowlist set. All countries are allowed.')
-    else:
-        await update.message.reply_text(f'Current allowlist: {db.get_allowlist()}')
-
-
 async def send_new_post(content, url, text):
     async with bot:
         await bot.send_message(configuration_values.CHAT_ID, content, parse_mode="HTML", read_timeout=20,
                                write_timeout=20,
                                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text=text, url=url)]]))
-
-
-def get_user_country(profile_id):
-    # Users are shared between all Vinted platforms, so we can use any of them
-    url = f"https://www.vinted.fr/api/v2/users/{profile_id}?localize=false"
-    response = requester.get(url)
-    # That's a LOT of requests, so if we get a 429 we wait a bit before retrying once
-    if response.status_code == 429:
-        # In case of rate limit, we're switching the endpoint. This one is slower, but it doesn't RL as soon. 
-        # We're limiting the items per page to 1 to grab as little data as possible
-        url = f"https://www.vinted.fr/api/v2/users/{profile_id}/items?page=1&per_page=1"
-        response = requester.get(url)
-        try:
-            user_country = response.json()["items"][0]["user"]["country_iso_code"]
-        except KeyError:
-            print("Couldn't get the country due to too many requests. Returning default value.")
-            user_country = "XX"
-    else:
-        user_country = response.json()["user"]["country_iso_code"]
-    return user_country
 
 
 async def process_items():
@@ -227,12 +163,6 @@ async def clear_item_queue(context: ContextTypes.DEFAULT_TYPE):
             # If already in db, pass
             elif db.is_item_in_db(id) != 0:
                 pass
-            # If there's an allowlist and
-            # If the user's country is not in the allowlist, we add it to the db and do nothing else
-            elif db.get_allowlist() != 0 and (get_user_country(item.raw_data["user"]["id"])) not in (
-                    db.get_allowlist() + ["XX"]):
-                db.add_item_to_db(id, query)
-                pass
             else:
                 # We create the message
                 content = configuration_values.MESSAGE.format(
@@ -253,11 +183,6 @@ async def set_commands(context: ContextTypes.DEFAULT_TYPE):
         ("add_query", "Add a keyword to the bot"),
         ("remove_query", "Remove a keyword from the bot"),
         ("queries", "List all keywords"),
-        ("create_allowlist", "Create an allowlist"),
-        ("delete_allowlist", "Delete the allowlist"),
-        ("add_country", "Add a country to the allowlist"),
-        ("remove_country", "Remove a country from the allowlist"),
-        ("allowlist", "List all countries in the allowlist")
     ])
 
 
@@ -278,12 +203,6 @@ app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("add_query", add_query))
 app.add_handler(CommandHandler("remove_query", remove_query))
 app.add_handler(CommandHandler("queries", queries))
-# Allowlist handlers
-app.add_handler(CommandHandler("create_allowlist", create_allowlist))
-app.add_handler(CommandHandler("delete_allowlist", delete_allowlist))
-app.add_handler(CommandHandler("add_country", add_country))
-app.add_handler(CommandHandler("remove_country", remove_country))
-app.add_handler(CommandHandler("allowlist", allowlist))
 
 # TODO : Help command
 
